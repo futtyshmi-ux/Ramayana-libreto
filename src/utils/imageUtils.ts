4a7d6adecd0e7c132f5c8635f9b72dd409e74070
DERNI <futtyshmi@gmail.com>
2026-07-28 08:52:49 +0300

    3

--- src/utils/imageUtils.ts
+++ src/utils/imageUtils.ts
@@ -1,13 +1,8 @@
 export function getImageUrl(image: any): string {
   if (!image) return '';
-  if (typeof image === 'string') return image;
-  if (typeof image === 'object' && image !== null) {
-    if ('default' in image && typeof image.default === 'string') {
-      return image.default;
-    }
-    if ('src' in image && typeof image.src === 'string') {
-      return image.src;
-    }
+  let url = typeof image === 'string' ? image : (image?.default || image?.src || String(image));
+  if (url.startsWith('/')) {
+    url = '.' + url;
   }
-  return String(image);
+  return url;
 }
